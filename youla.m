351e7b3370860a20c14a3fd3ef365e91388a753c
Kaleb Bordner <kaleb.bordner@gmail.com>
2024-06-07 13:58:24 -0700

    adding youla file

---
+++ youla.m
@@ -0,0 +1,56 @@
+clearvars
+load("saved_trim_states/5_alpha_trim.mat")
+sys_as_tf=tf(ac.mimo_system);
+
+s = sym('s');
+tfm = [-0.8747*s-3.0357,8.1051*s-0.6938;0.3095*s+0.0036,0.0707*s+0.3976];
+
+[N,del] = ss2tfm(ac.mimo_system.A, ac.mimo_system.B, ac.mimo_system.C, ac.mimo_system.D,1);
+N2 = vpa(poly2sym(N),2);
+del = vpa(poly2sym(del),2);
+
+[U_l,U_r,Mp] = smithForm(tfm,s);
+Mp = Mp/del;
+
+%%
+%clear vars
+%syms('s');
+s = tf('s');
+
+UL = [0 1;1 -21*s];
+UR = [-0.58 -0.028*s-0.15; 2.5 0.12*s+1.4e-3];
+del = s^2+0.15*s+0.47; % manual effort to change this for each system
+Mpdel = [1 0; 0 (s^2+0.15*s+0.47)];
+Mp = vpa(Mpdel/del, 2);
+
+Y1 = 0.47;
+Y2 = 0.47/del;
+MP = [Y1 0; 0 Y2];
+MT = vpa(MP*MY, 2);
+
+Ty = inv(UL) * MT * UL;
+Ty = vpa(Ty, 2);
+
+M_T = Mp*M_y;
+T_y = inv(U_l)*M_T*U_l;
+S_y = 1-T_y;
+G_C = U_r*inv(eye(2)-M_T)*M_y*U_l;
+
+%%
+% Y = U_r*M_y*U_l
+% G_C = U_r*inv(eye(2)-M_T)*M_y*U_l;
+
+
+
+% Gc11 = -(473387*(10000*s^2 + 1466*s + 4687))/(3508648*(5000*s^2 + 733*s)*(s^2 + (733*s)/5000 + 4687/10000));
+% Gc12 = -(4687*((3535000*s)/128518487 + 19880000/128518487)*(10000*s^2 + 1466*s + 4687))/(20000*(5000*s^2 + 733*s)*(s^2 + (733*s)/5000 + 4687/10000));
+% Gc21 = (14506265*(10000*s^2 + 1466*s + 4687))/(24560536*(5000*s^2 + 733*s)*(s^2 + (733*s)/5000 + 4687/10000));
+% Gc22 = (4687*((15475000*s)/128518487 + 180000/128518487)*(10000*s^2 + 1466*s + 4687))/(20000*(5000*s^2 + 733*s)*(s^2 + (733*s)/5000 + 4687/10000));
+% G_C = [Gc11 Gc12; Gc21 Gc22];
+
+
+Ly = sys_as_tf * G_C;
+T_y = (eye(2) + Ly)^(-1) * Ly; % Complementary sensitivity function
+Y = G_C * (eye(2) + Ly)^(-1);
+
+bode(T_y)
